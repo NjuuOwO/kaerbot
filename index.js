c9bfb2aef0ded90551e4c2758c8c7f63fa3acd38
NjuuOwO <45913099+NjuuOwO@users.noreply.github.com>
2020-07-02 21:53:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ bot.on("ready", () => {
 
 
 bot.on('message', msg => {
-	if(msg.author.bot) return;
-	if(msg.channel.id != "725340463782953072") return;
+	if(msg.author.bot || msg.channel.id != "725340463782953072") return;
 
 	const args = msg.content.slice(prefix.length).split(/ +/);
 	const cmd = args.shift().toLowerCase();
